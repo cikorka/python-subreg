1cf31c40d0837c974a20d0d6b26f04fadff20a1e
Petr Jeřábek <cikorka@me.com>
2013-11-11 00:53:59 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -2,17 +2,29 @@
 
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='python-subreg',
-    version='0.0.3',
+    version='0.0.4',
     description='Python wrapper around the subreg.cz SOAP API',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='subreg subreg.cz',
     url='http://github.com/cikorka/python-subreg',
-    author='cikorka',
+    author='Petr Jerabek',
     author_email='cikorka@me.com',
     license='MIT',
     packages=['subreg'],
     install_requires=[
         'SOAPpy',
     ],
-    zip_safe=False
+    include_package_data=True,
+    zip_safe=False,
 )
