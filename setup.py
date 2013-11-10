71b842c76b183e4dc54f43a877e348c2fef3806c
Petr Jeřábek <cikorka@me.com>
2013-11-10 13:01:57 +0100

    updated description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='python-subreg',
     version='0.0.1',
-    description='The funniest joke in the world',
+    description='Python wrapper around the subreg.cz SOAP API',
     url='http://github.com/cikorka/python-subreg',
     author='cikorka',
     author_email='cikorka@me.com',
