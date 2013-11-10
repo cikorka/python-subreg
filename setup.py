23e17cc6b2e2eab452ce1eccc97aadf2670eca09
Petr Jeřábek <cikorka@me.com>
2013-11-10 03:12:44 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='python-subreg',
+    version='0.0.1',
+    description='The funniest joke in the world',
+    url='http://github.com/cikorka/python-subreg',
+    author='cikorka',
+    author_email='cikorka@me.com',
+    license='MIT',
+    packages=['subreg'],
+    install_requires=[
+        'SOAPpy',
+    ],
+    zip_safe=False
+)
